07b9f3ff3617ae64b5947406948a4926fa37f326
ffelipesimoes <ffelipesimoes@users.noreply.github.com>
2018-07-14 23:26:02 -0300

    Add_new_pages_v001

---
+++ routes/page1.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/page1', function(req, res, next) {
+  res.render('page1', { title: 'Express' });
+});
+
+module.exports = router;
