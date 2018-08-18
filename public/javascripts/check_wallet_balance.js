dc8fc3240d3af0eef04f2dc57ebc4d6486647d50
ffelipesimoes <ffelipesimoes@users.noreply.github.com>
2018-08-18 20:13:13 -0300

    update_site_v011

--- public/javascripts/check_wallet_balance.js
+++ public/javascripts/check_wallet_balance.js
@@ -1,4 +1,4 @@
-function check_wallet_balance() {
+function check_wallet_balance () {
     //function to get the query string
     function getParameterByName(name, url) {
         if (!url) url = window.location.href;
@@ -10,56 +10,75 @@ function check_wallet_balance() {
         return decodeURIComponent(results[2].replace(/\+/g, ' '));
     }
     //--------------------------------------
-
-
-    var hash = getParameterByName('hash_result');
+    
+    
+    var hash = getParameterByName('hash_result'); 
     var request = new XMLHttpRequest();
     var wallet = document.getElementById("newwallet").value;
     var Pkey = document.getElementById("pkey").value;
-
-
-
-    request.open('GET', 'https://api.blockcypher.com/v1/btc/test3/addrs/' + wallet, true);
+    
+    
+    
+    request.open('GET', 'https://api.blockcypher.com/v1/btc/test3/addrs/'+ wallet, true);
     request.onload = function () {
 
-        // Begin accessing JSON data here
+         // Begin accessing JSON data here
         var data = JSON.parse(this.response);
-        if (request.status >= 200 && request.status < 400) {
-            if (data.balance > 0.00002500) {
-                console.log('SALDO OK')
-                console.log("Wallet: " + wallet);
-                console.log("PKey: " + Pkey)
-                console.log("hash: " + hash)
+            if (request.status >= 200 && request.status < 400 ) 
+                {
+                   if (data.balance > 0.00002500) {
+                    console.log('SALDO OK')
+                    console.log("Wallet: " + wallet);
+                    console.log("PKey: " + Pkey)
+                    console.log("hash: " + hash)
 
-                var privateKey = new bitcore.PrivateKey(Pkey);
-                var utxo = {
-                    "txId": "115e8f72f39fad874cfab0deed11a80f24f967a84079fb56ddf53ea02e308986",
-                    "outputIndex": 0,
-                    "address": wallet,
-                    "script": "76a91447862fe165e6121af80d5dde1ecb478ed170565b88ac",
-                    "satoshis": 50000
-                };
+                   
+                    var Insight = require('bitcore-explorers').Insight;
+                    var Insight = new Insight('testnet');
 
-                var transaction = new bitcore.Transaction()
-                    .from(utxo)
-                    .addData(hash) // Add OP_RETURN data
-                    .sign(privateKey)
-                    .serialize();
-                console.log("deu certo")    
+                    Insight.getUnspentUtxos(wallet, function(err, utxos) {
+                        if (err) {
+                        // Handle errors...
+                            console.log('error getting unspent outputs')
+                        } else {
+                        // use the UTXOs to create a transaction
+                        //console.log(utxos);
+                        var tx = bitcore.Transaction();
+                        tx.from(utxos);
+                        tx.to(wallet, 10000); // .0001 BTC
+                        tx.change(wallet); // recommend new address here for better privacy
+                        tx.fee(50000);
+                        tx.addData('Leticia Simões Eu te amo, by:Felipe Simões')
+                        tx.sign(Pkey);
 
-            }
+                        tx.serialize();
+    
 
-            else {
-                console.log('Saldo insuficiente')
-            }
+                        // broadcast using bitpay's insight server
+                        Insight.broadcast(tx, function(err, returnedTxId) {
+                            if (err) {
+                            // Handle errors...
+                                console.log(err);
+                            } else {
+                             // Mark the transaction as broadcasted
+                            console.log('sucessful broadcast: ' + returnedTxId);
+                        }
+                    });
+                }
+            });
 
-        } else {
-            console.log('error');
+                   }else
+                   {
+                       console.log('Saldo insuficiente')
+                   }            
+
+                } else 
+                {
+                    console.log('error');
+                }
         }
-    }
 
-    request.send();
+     request.send();
+       
+ }
 
-
-
-}
---
+++ public/javascripts/check_wallet_balance_test.js
@@ -0,0 +1,65 @@
+function check_wallet_balance() {
+    //function to get the query string
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, '\\$&');
+        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    }
+    //--------------------------------------
+
+
+    var hash = getParameterByName('hash_result');
+    var request = new XMLHttpRequest();
+    var wallet = document.getElementById("newwallet").value;
+    var Pkey = document.getElementById("pkey").value;
+
+
+
+    request.open('GET', 'https://api.blockcypher.com/v1/btc/test3/addrs/' + wallet, true);
+    request.onload = function () {
+
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response);
+        if (request.status >= 200 && request.status < 400) {
+            if (data.balance > 0.00002500) {
+                console.log('SALDO OK')
+                console.log("Wallet: " + wallet);
+                console.log("PKey: " + Pkey)
+                console.log("hash: " + hash)
+
+                var privateKey = new bitcore.PrivateKey(Pkey);
+                var utxo = {
+                    "txId": "115e8f72f39fad874cfab0deed11a80f24f967a84079fb56ddf53ea02e308986",
+                    "outputIndex": 0,
+                    "address": wallet,
+                    "script": "76a91447862fe165e6121af80d5dde1ecb478ed170565b88ac",
+                    "satoshis": 50000
+                };
+
+                var transaction = new bitcore.Transaction()
+                    .from(utxo)
+                    .addData(hash) // Add OP_RETURN data
+                    .sign(privateKey)
+                    .serialize();
+                console.log("deu certo")    
+
+            }
+
+            else {
+                console.log('Saldo insuficiente')
+            }
+
+        } else {
+            console.log('error');
+        }
+    }
+
+    request.send();
+
+
+
+}
